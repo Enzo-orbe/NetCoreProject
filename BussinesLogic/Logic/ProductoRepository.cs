583d7bf38dff295679b0780dc6a47aa8514fdf24
Enzo Facundo Orbe <Enzo.Orbe@tsoftglobal.com>
2021-11-25 17:13:30 -0300

    Instalacion de dependencias y creacion de interfaces

---
+++ BussinesLogic/Logic/ProductoRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
+
+namespace BussinesLogic.Logic
+{
+    public class ProductoRepository : IProductoRepository
+    {
+        public Task<IReadOnlyList<Producto>> GetProductoAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Producto> GetProductoByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BussinesLogic.Logic;
+using Core.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -24,7 +26,7 @@ namespace WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddTransient<IProductoRepository, ProductoRepository>();
             services.AddControllers();
         }
 
