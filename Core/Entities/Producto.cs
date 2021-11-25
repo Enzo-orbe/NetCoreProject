ad058b552c25b8c24809bcbf46897ccd62db7bb7
Enzo Facundo Orbe <Enzo.Orbe@tsoftglobal.com>
2021-11-25 17:24:35 -0300

    Creacion de Clases

--- Core/Entities/Producto.cs
+++ Core/Entities/Producto.cs
@@ -7,9 +7,8 @@ using System.Threading.Tasks;
 
 namespace Core.Entities
 {
-   public class Producto
+    public class Producto : ClaseBase
     {
-        public int Id { get; set; }
 
         public string Nombre { get; set; }
 
@@ -17,7 +16,13 @@ namespace Core.Entities
 
         public int Stock { get; set; }
 
-        public string Marca { get; set; }
+        public int MarcaId { get; set; }
+
+        public Marca Marca { get; set; }
+
+        public int CategoriaId { get; set; }
+
+        public Categoria Categoria {get; set;}
 
         [Column(TypeName = "decimal(18,4)")]
         public decimal Precio { get; set; }
